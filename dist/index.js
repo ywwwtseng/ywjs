1b88df182cfa4440bc82c5bc2821793ec5c946c3
William <ywwwtseng@gmail.com>
2025-11-15 15:23:43 +0800

    fix: fix validate and allowed function

--- dist/index.js
+++ dist/index.js
@@ -184,26 +184,32 @@ var validate = (params, schema) => {
   const keys = Object.keys(schema);
   for (let index = 0; index < keys.length; index++) {
     const key = keys[index];
-    if (params[key] === void 0) {
+    if (schema[key].required && params[key] === void 0) {
       return {
         error: `Parameter (${key}) is required`
       };
-    } else if (!(schema[key].nullable && params[key] === null) && schema[key].type !== "enum" && typeof params[key] !== schema[key].type) {
+    } else if (!(schema[key].nullable && params[key] === null) && !(!schema[key].required && params[key] === void 0) && schema[key].type !== "enum" && typeof params[key] !== schema[key].type) {
       return {
-        error: `Parameter (${key}) type need ${schema[key]}`
+        error: `Parameter (${key}) type need ${schema[key].type}`
       };
     } else if (schema[key].type === "enum" && !schema[key].enum?.includes(params[key])) {
       return {
-        error: `Parameter (${key}) need one of (${schema[key].toString()}), but got ${params[key]}`
+        error: `Parameter (${key}) need one of (${schema[key].enum?.join(
+          ", "
+        )}), but got ${params[key]}`
       };
     } else if (schema[key].required) {
       if (schema[key].type === "string" && typeof params[key] === "string" && params[key].trim() === "") {
         return {
-          error: `Parameter (${key}) is required`
+          error: `Parameter (${key}) can't be empty string`
         };
       } else if (schema[key].type === "number" && typeof params[key] === "number" && params[key] === 0) {
         return {
-          error: `Parameter (${key}) is required`
+          error: `Parameter (${key}) can't be 0`
+        };
+      } else if (schema[key].type === "boolean" && typeof params[key] !== "boolean") {
+        return {
+          error: `Parameter (${key}) type need boolean, but got ${params[key]}`
         };
       }
     }
@@ -222,11 +228,25 @@ var allowed = (params, schema) => {
       };
     } else if (!(schema[key].nullable && params[key] === null) && schema[key].type !== "enum" && typeof params[key] !== schema[key].type) {
       return {
-        error: `Parameter (${key}) type need ${schema[key]}`
+        error: `Parameter (${key}) type need ${schema[key].type}`
       };
     } else if (schema[key].type === "enum" && !schema[key].enum?.includes(params[key])) {
       return {
-        error: `Parameter (${key}) need one of (${schema[key].toString()}), but got ${params[key]}`
+        error: `Parameter (${key}) need one of (${schema[key].enum?.join(
+          ", "
+        )}), but got ${params[key]}`
+      };
+    } else if (schema[key].type === "string" && typeof params[key] !== "string") {
+      return {
+        error: `Parameter (${key}) type need string, but got ${params[key]}`
+      };
+    } else if (schema[key].type === "number" && typeof params[key] !== "number") {
+      return {
+        error: `Parameter (${key}) type need number, but got ${params[key]}`
+      };
+    } else if (schema[key].type === "boolean" && typeof params[key] !== "boolean") {
+      return {
+        error: `Parameter (${key}) type need boolean, but got ${params[key]}`
       };
     }
   }
--- src/params.ts
+++ src/params.ts
@@ -14,26 +14,27 @@ export const validate = (
   for (let index = 0; index < keys.length; index++) {
     const key = keys[index];
 
-    if (params[key] === undefined) {
+    if (schema[key].required && params[key] === undefined) {
       return {
         error: `Parameter (${key}) is required`,
       };
     } else if (
       !(schema[key].nullable && params[key] === null) &&
+      !(!schema[key].required && params[key] === undefined) &&
       schema[key].type !== 'enum' &&
       typeof params[key] !== schema[key].type
     ) {
       return {
-        error: `Parameter (${key}) type need ${schema[key]}`,
+        error: `Parameter (${key}) type need ${schema[key].type}`,
       };
     } else if (
       schema[key].type === 'enum' &&
       !schema[key].enum?.includes(params[key] as string)
     ) {
       return {
-        error: `Parameter (${key}) need one of (${schema[
-          key
-        ].toString()}), but got ${params[key]}`,
+        error: `Parameter (${key}) need one of (${schema[key].enum?.join(
+          ', '
+        )}), but got ${params[key]}`,
       };
     } else if (schema[key].required) {
       if (
@@ -42,7 +43,7 @@ export const validate = (
         params[key].trim() === ''
       ) {
         return {
-          error: `Parameter (${key}) is required`,
+          error: `Parameter (${key}) can't be empty string`,
         };
       } else if (
         schema[key].type === 'number' &&
@@ -50,7 +51,14 @@ export const validate = (
         params[key] === 0
       ) {
         return {
-          error: `Parameter (${key}) is required`,
+          error: `Parameter (${key}) can't be 0`,
+        };
+      } else if (
+        schema[key].type === 'boolean' &&
+        typeof params[key] !== 'boolean'
+      ) {
+        return {
+          error: `Parameter (${key}) type need boolean, but got ${params[key]}`,
         };
       }
     }
@@ -86,16 +94,37 @@ export const allowed = (
       typeof params[key] !== schema[key].type
     ) {
       return {
-        error: `Parameter (${key}) type need ${schema[key]}`,
+        error: `Parameter (${key}) type need ${schema[key].type}`,
       };
     } else if (
       schema[key].type === 'enum' &&
       !schema[key].enum?.includes(params[key] as string)
     ) {
       return {
-        error: `Parameter (${key}) need one of (${schema[
-          key
-        ].toString()}), but got ${params[key]}`,
+        error: `Parameter (${key}) need one of (${schema[key].enum?.join(
+          ', '
+        )}), but got ${params[key]}`,
+      };
+    } else if (
+      schema[key].type === 'string' &&
+      typeof params[key] !== 'string'
+    ) {
+      return {
+        error: `Parameter (${key}) type need string, but got ${params[key]}`,
+      };
+    } else if (
+      schema[key].type === 'number' &&
+      typeof params[key] !== 'number'
+    ) {
+      return {
+        error: `Parameter (${key}) type need number, but got ${params[key]}`,
+      };
+    } else if (
+      schema[key].type === 'boolean' &&
+      typeof params[key] !== 'boolean'
+    ) {
+      return {
+        error: `Parameter (${key}) type need boolean, but got ${params[key]}`,
       };
     }
   }
